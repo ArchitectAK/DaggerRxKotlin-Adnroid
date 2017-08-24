42f101330b712041ae4d2826960aed013298db52
AnkitDroidGit <urmailing2ankit@gmail.com>
2017-08-24 17:06:35 +0800

    -login screen

---
+++ app/src/main/java/com/freeankit/dagger2retrofit2rxkotlinrxandroidapplication/ui/LoginActivity.kt
@@ -0,0 +1,18 @@
+package com.freeankit.dagger2retrofit2rxkotlinrxandroidapplication.ui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.freeankit.dagger2retrofit2rxkotlinrxandroidapplication.AppAnkit
+import com.freeankit.dagger2retrofit2rxkotlinrxandroidapplication.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+/**
+ * @author Ankit Kumar (ankitdroiddeveloper@gmail.com)
+ * @date 24/08/2017.
+ */
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
--- app/src/main/java/com/freeankit/dagger2retrofit2rxkotlinrxandroidapplication/ui/MainFragment.kt
+++ app/src/main/java/com/freeankit/dagger2retrofit2rxkotlinrxandroidapplication/ui/MainFragment.kt
@@ -1,12 +1,14 @@
 package com.freeankit.dagger2retrofit2rxkotlinrxandroidapplication.ui
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.freeankit.dagger2retrofit2rxkotlinrxandroidapplication.AppAnkit
@@ -32,6 +34,9 @@ class MainFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val view = inflater!!.inflate(R.layout.fragment_main, container, false)
+        view.findViewById<TextView>(R.id.name)?.setOnClickListener({
+            startActivity(Intent(context, LoginActivity::class.java))
+        })
         requestUser()
         return view
     }
