def6697e4219bed8e2d326769f7b893eb423f158
AnkitDroidGit <urmailing2ankit@gmail.com>
2018-01-04 13:42:40 +0800

    -updated fragment

--- app/src/main/java/com/freeankit/dagger2retrofit2rxkotlinrxandroidapplication/ui/MainActivity.kt
+++ app/src/main/java/com/freeankit/dagger2retrofit2rxkotlinrxandroidapplication/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.freeankit.dagger2retrofit2rxkotlinrxandroidapplication.ui
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.freeankit.dagger2retrofit2rxkotlinrxandroidapplication.AppAnkit
@@ -8,27 +9,14 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("CommitTransaction")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        AppAnkit.appComponent.inject(this);
+        AppAnkit.appComponent.inject(this)
         val toolbar = toolbar as android.support.v7.widget.Toolbar
         setSupportActionBar(toolbar)
-        supportActionBar?.setDisplayShowTitleEnabled(false);
-        toolbar.setBackgroundColor(resources.getColor(R.color.colorAccent));
-    }
-
-    fun fibonacci() {
-        val febCount = 15
-        val feb = IntArray(febCount)
-        feb[0] = 0
-        feb[1] = 1
-        for (i in 2 until febCount) {
-            feb[i] = feb[i - 1] + feb[i - 2]
-        }
-
-        for (i in 0 until febCount) {
-            print(feb[i].toString() + " ")
-        }
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        toolbar.setBackgroundColor(resources.getColor(R.color.colorAccent))
     }
 }
